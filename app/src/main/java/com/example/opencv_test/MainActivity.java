e9da78f38e90606365d7dc45280f4f7379636ba5
PtspluS <pierre.marez@isen.yncrea.fr>
2020-04-11 17:00:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opencv_test/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.opencv_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(R.string.app_name);
+
+        /*
+        AdView adView = (AdView) this.findViewById(R.id.adViewMain);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("TEST_DEVICE_ID")
+                .build();
+        adView.loadAd(adRequest);
+        */
+
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
+
+        Button btnNewProject = findViewById(R.id.buttonNewProject);
+        Button btnOpenProject = findViewById(R.id.buttonOpenProject);
+
+        btnNewProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                intent.putExtra("General Map Picture", true);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/opencv_test/Piece.java
@@ -0,0 +1,13 @@
+package com.example.opencv_test;
+
+import android.media.Image;
+
+public class Piece {
+    private String pathImage;
+    private Image image;
+
+    public Piece(){
+        this.pathImage = null;
+        this.image = null;
+    }
+}
